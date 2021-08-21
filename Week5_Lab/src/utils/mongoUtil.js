8db60889de5f9ac0215e8cb00c3f7f6f97b0fd71
KarlokCheung <georgecheung07@gmail.com>
2021-08-21 23:20:23 +1000

    add mongoDB setup

---
+++ Week5_Lab/src/utils/mongoUtil.js
@@ -0,0 +1,24 @@
+const mongodb = require('mongodb');
+
+const { MongoClient } = mongodb;
+
+const logger = console;
+
+let db;
+
+module.exports = {
+  connectToDb: () => {
+    const dbName = process.env.DB_NAME;
+    const url = process.env.CONNECTION_URL;
+
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+      if (err) {
+        logger.log('Err  ', err);
+      } else {
+        logger.log('Connected successfully to server');
+        db = client.db(dbName);
+      }
+    });
+  },
+  getDb: () => db,
+};
