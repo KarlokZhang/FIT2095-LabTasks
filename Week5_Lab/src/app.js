d8ec593b98f43b6342d8a38f1e96b9be4ea98d67
KarlokCheung <georgecheung07@gmail.com>
2021-08-21 19:02:46 +1000

    add server app

--- Week5_Lab/src/app.js
+++ Week5_Lab/src/app.js
@@ -1,1 +1,26 @@
 require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const router = require('./router');
+const logger = console;
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+const morganLog =
+  process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev');
+
+app.use(morganLog);
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  logger.log(`server listening on port ${PORT}`);
+});
