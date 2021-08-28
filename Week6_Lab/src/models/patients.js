4b13bcf7166fd0c5522493a24f45169da0374435
KarlokCheung <georgecheung07@gmail.com>
2021-08-28 12:26:01 +1000

    add(model): add patients schema

---
+++ Week6_Lab/src/models/patients.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  age: {
+    type: Number,
+    required: true,
+    validate: {
+      validator: function (age) {
+        return age >= 0 && age <= 120;
+      },
+      message: 'Age must be a number between 0 and 120',
+    },
+  },
+  dateOfVisit: {
+    type: Date,
+    default: Date.now,
+  },
+  caseDescription: {
+    type: String,
+    minlength: 10,
+  },
+  doctorId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Doctor',
+  },
+});
+
+module.exports = model('Patient', schema);
