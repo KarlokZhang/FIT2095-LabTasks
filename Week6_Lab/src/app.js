326a24c877269b178b8f7c06ef164bb00c8fd058
KarlokCheung <georgecheung07@gmail.com>
2021-08-28 20:29:56 +1000

    add(app): config view engine

--- Week6_Lab/src/app.js
+++ Week6_Lab/src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const router = require('./routes');
 
@@ -12,8 +13,13 @@ const morganLog =
 
 app.use(morganLog);
 app.use(cors());
-
 app.use(express.json());
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.use('/', router);
 
 module.exports = app;
