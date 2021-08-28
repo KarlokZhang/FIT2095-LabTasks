12a8004464023d41479120a6b7feac02c994b243
KarlokCheung <georgecheung07@gmail.com>
2021-08-28 14:12:37 +1000

    add(controller): add patients controller

---
+++ Week6_Lab/src/controllers/patients.js
@@ -0,0 +1,108 @@
+const Doctor = require('../models/doctors');
+const Patient = require('../models/patients');
+
+async function getAllPatients(req, res) {
+  const patients = await Patient.find().exec();
+  if (!patients) {
+    return res.sendStatus(404);
+  }
+  return patients;
+}
+
+async function getPatientById(req, res) {
+  const { id } = req.params;
+  try {
+    const patient = await Patient.findById(id).exec();
+    return patient;
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(404);
+  }
+}
+
+async function createPatient(req, res) {
+  const { firstName, lastName, age, dateOfVisit, description, doctorId } =
+    req.body;
+
+  try {
+    const patient = new Patient({
+      fullName: {
+        firstName,
+        lastName,
+      },
+      age: age,
+      dateOfVisit: dateOfVisit,
+      caseDescription: description,
+      doctorId: doctorId,
+    });
+    const result = await patient.save();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updatePatientById(req, res) {
+  const { id } = req.params;
+  const { firstName, lastName, age, dateOfVisit, description, doctorId } =
+    req.body;
+
+  // 1. find patient by id
+  // 2. find doctor by id
+  // 3. update patient
+  // 4. check if patient change new doctor
+  //    - yes: update doctor patient number (+ || -)
+  //    - no: return
+
+  const patient = await Patient.findById(id);
+  const currentDoctor = await Doctor.findById(patient.doctorId);
+  const doctor = await Doctor.findById(doctorId);
+
+  if (!patient || !doctor || !currentDoctor) {
+    return res.sendStatus(404);
+  }
+
+  try {
+    const patient = await Patient.findByIdAndUpdate(id, {
+      fullName: {
+        firstName,
+        lastName,
+      },
+      age: age,
+      dateOfVisit: dateOfVisit,
+      caseDescription: description,
+      doctorId: doctorId,
+    }).exec();
+
+    // check if change doctor
+    if (patient.doctorId !== doctorId) {
+      currentDoctor.numPatients -= 1;
+      doctor.numPatients += 1;
+      await currentDoctor.save();
+      await doctor.save();
+    }
+    return patient;
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(404);
+  }
+}
+
+async function deletePatientById(req, res) {
+  const { id } = req.params;
+  try {
+    const patient = await Patient.findByIdAndDelete(id).exec();
+    return res.sendStatus(204);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(404);
+  }
+}
+
+module.exports = {
+  getAllPatients,
+  getPatientById,
+  createPatient,
+  updatePatientById,
+  deletePatientById,
+};
