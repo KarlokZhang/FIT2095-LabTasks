3b5ea9fdf3636d0aad4e2b987d0c1f49d1579b0e
KarlokCheung <georgecheung07@gmail.com>
2021-08-30 15:21:10 +1000

    fix .env file

--- Week6_Lab/index.js
+++ Week6_Lab/index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: './.env' });
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const app = require('./src/app');
 const { connectToDB } = require('./src/utils/mongoUtil');
 const PORT = process.env.PORT || 8080;
