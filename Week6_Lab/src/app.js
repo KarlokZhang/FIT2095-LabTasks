7fbeb0211ef7108a336f6e0aba978382d3c6dfd8
KarlokCheung <georgecheung07@gmail.com>
2021-08-30 14:36:42 +1000

    set urlencoded to false

--- Week6_Lab/src/app.js
+++ Week6_Lab/src/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(
   express.urlencoded({
-    extended: true,
+    extended: false,
   }),
 );
 app.engine('html', require('ejs').renderFile);
