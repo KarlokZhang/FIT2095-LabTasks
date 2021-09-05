ee4b949261d6d7073091d0d4d8741e83b1294e18
KarlokCheung <georgecheung07@gmail.com>
2021-09-05 22:16:05 +1000

    add graphql and apollo server

--- Week7_Lab/src/app.js
+++ Week7_Lab/src/app.js
@@ -3,10 +3,34 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const router = require('./routes');
 
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world333333!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+});
+
 const app = express();
+
+server.applyMiddleware({ app });
 
 const morganLog =
   process.env.NODE_ENV === 'production' ? morgan('common') : morgan('dev');
