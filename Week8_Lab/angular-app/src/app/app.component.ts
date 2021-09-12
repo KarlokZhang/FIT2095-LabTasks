52af2107e8e00c1dbd11efb3822e755cd3af78a8
KarlokCheung <georgecheung07@gmail.com>
2021-09-12 18:57:34 +1000

    add doctor table

--- Week8_Lab/angular-app/src/app/app.component.ts
+++ Week8_Lab/angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,47 @@ import { Component } from '@angular/core';
 export class AppComponent {
   firstName: string = '';
   lastName: string = '';
-  dateOfBirth: Date = new Date();
+  dateOfBirth: Date | null = null;
   suburb: string = '';
   state: string = '';
-  postCode: number = 0;
-  numOfPatients: number = 0;
+  postCode: number | null = null;
+  numOfPatients: number | null = null;
   doctorsDb: any[] = [];
+
+  saveDoctor(): void {
+    this.doctorsDb.push({
+      id: uuidv4(),
+      firstName: this.firstName,
+      lastName: this.lastName,
+      dateOfBirth: this.dateOfBirth,
+      suburb: this.suburb,
+      state: this.state,
+      postCode: this.postCode,
+      numOfPatients: this.numOfPatients,
+      address: this.formattedAddress(this.suburb, this.state, this.postCode),
+    });
+
+    console.log(this.doctorsDb);
+
+    this.firstName = '';
+    this.lastName = '';
+    this.dateOfBirth = null;
+    this.suburb = '';
+    this.state = '';
+    this.postCode = null;
+    this.numOfPatients = null;
+  }
+
+  formattedAddress(
+    suburb: string,
+    state: string,
+    postCode: number | null
+  ): string {
+    return `${this.capitalizeString(suburb)}, ${state}, ${postCode}`;
+  }
+
+  capitalizeString(str: string): string {
+    const lower = str.toLowerCase();
+    return str.charAt(0).toUpperCase() + lower.slice(1);
+  }
 }
