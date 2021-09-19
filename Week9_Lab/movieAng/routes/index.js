051a477693aeddd95c9becfe0f44d3c087577c7e
KarlokCheung <georgecheung07@gmail.com>
2021-09-19 16:25:21 +1000

    refactor app route to render dist app

--- Week9_Lab/movieAng/routes/index.js
+++ Week9_Lab/movieAng/routes/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const movieRoute = require('./movies');
-const actorRoute = require('./actors');
+const express = require("express");
+const movieRoute = require("./movies");
+const actorRoute = require("./actors");
 
 const router = express.Router();
 
-router.use('/movies', movieRoute);
-router.use('/actors', actorRoute);
+router.use("/movies", movieRoute);
+router.use("/actors", actorRoute);
 
 module.exports = router;
---
+++ Week9_Lab/movieAng/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const router = require("./routes");
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const morganLog =
+  process.env.NODE_ENV === "production" ? morgan("common") : morgan("dev");
+
+app.use(morganLog);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.use("/", express.static(path.join(__dirname, "dist/movieAng")));
+
+// app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
