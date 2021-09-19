76e5e4bea40e5a840b7ea80b821e0c011eb83422
KarlokCheung <georgecheung07@gmail.com>
2021-09-19 16:39:03 +1000

    add actor component

--- Week9_Lab/movieAng/src/app/app.module.ts
+++ Week9_Lab/movieAng/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ActorComponent } from './actor/actor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActorComponent
   ],
   imports: [
     BrowserModule
