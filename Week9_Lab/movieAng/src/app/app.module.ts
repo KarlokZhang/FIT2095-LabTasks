ec1ea03e18568d63fe1c4cda1097744efc63115e
KarlokCheung <georgecheung07@gmail.com>
2021-09-19 16:59:37 +1000

    add httpclient in database service

--- Week9_Lab/movieAng/src/app/app.module.ts
+++ Week9_Lab/movieAng/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ActorComponent } from './actor/actor.component';
+import { DatabaseService } from './database.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ActorComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ActorComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [DatabaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Week9_Lab/movieAng/src/app/database.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DatabaseService {
+  constructor(private http: HttpClient) {}
+  result: any;
+  getActors() {
+    return this.http.get('/actors');
+  }
+  getActor(id: string) {
+    let url = '/actors/' + id;
+    return this.http.get(url);
+  }
+  createActor(data) {
+    return this.http.post('/actors', data, httpOptions);
+  }
+  updateActor(id, data) {
+    let url = '/actors/' + id;
+    return this.http.put(url, data, httpOptions);
+  }
+  deleteActor(id) {
+    let url = '/actors/' + id;
+    return this.http.delete(url, httpOptions);
+  }
+}
